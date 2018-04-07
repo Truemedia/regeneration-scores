b780c6c2344a7f09d5946c0b03dec3db4e3c0753
Wade Penistone <wadespackages@yahoo.com>
2018-04-07 16:36:33 +0100

    PSR-4 boilerplate

---
+++ src/ScoresServiceProvider.php
@@ -0,0 +1,16 @@
+<?php namespace Regeneration\Scores;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScoresServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Add code here
+    }
+}
